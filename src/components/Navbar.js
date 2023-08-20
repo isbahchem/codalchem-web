aa28b7b64284902ffde37e55413fb8a3164fb78e
Isbah Ahmed Feroz <64509937+isbahchem@users.noreply.github.com>
2023-08-20 19:46:20 +0500

    Navbar Style

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,22 +4,30 @@ function Navbar(){
     const myNav = {
         display: "flex",
         color: "white",
+        margin: "12px",
+        padding: "12px",
         backgroundColor: "DodgerBlue",
-        padding: "5px",
         fontFamily: "Arial",
         listStyle: "none",
         borderRadius: "25px"
       };
+      const anchor = {
+        margin: "2px",
+        padding: "5px",
+        backgroundColor: "LightBlue",
+        borderRadius: "15px",
+        color: "Black"
+      }
     return(
         <>
         <nav>
         <div>
             <ul style={myNav}>
-                <li>Home</li>
-                <li>About</li>
-                <li>Computer Science</li>
-                <li>Programming Languages</li>
-                <li>Chemistry</li>
+                <li><a style={anchor}>Home</a></li>
+                <li><a style={anchor}>About</a></li>
+                <li><a style={anchor}>Computer Science</a></li>
+                <li><a style={anchor}>Programming Languages</a></li>
+                <li><a style={anchor}>Chemistry</a></li>
             </ul>
         </div>
     </nav>
