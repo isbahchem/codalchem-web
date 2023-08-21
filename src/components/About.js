73e449f99b1f9f76f6c3d707089f9bb2cd5c45f2
Isbah Ahmed Feroz <64509937+isbahchem@users.noreply.github.com>
2023-08-21 20:31:59 +0500

    About component created

---
+++ src/components/About.js
@@ -0,0 +1,10 @@
+
+function About(){
+    return(
+        <>
+        <h2>About CodalChem</h2>
+        <p style={{color: "Blue"}}>This Website is about programming and Chemistry.</p>
+        
+        </>
+    )
+}
