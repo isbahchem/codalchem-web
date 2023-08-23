6c94a85a5f674e8e3c37b7f238887474128e8dc8
Isbah Ahmed Feroz <64509937+isbahchem@users.noreply.github.com>
2023-08-23 20:32:13 +0500

    about edited

--- src/components/About.js
+++ src/components/About.js
@@ -4,7 +4,9 @@ function About(){
         <>
         <h2>About CodalChem</h2>
         <p style={{color: "Blue"}}>This Website is about programming and Chemistry.</p>
-        
+        <p style={{color: "darkcyan"}}>Chemistry is the branch of science which deals with the substances of which matter is composed, the investigation of their properties and reactions, and the use of such reactions to form new substances.</p>
+        <p style={{color: "darkgreen"}}>Computer science focuses on the development and testing of software and software systems. It involves working with mathematical models, data analysis and security, algorithms, and computational theory.</p>
         </>
     )
 }
+export default About;
